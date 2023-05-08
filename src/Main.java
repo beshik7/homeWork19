9d3267b8afea9f1e3fc050d963acc57d0b451b1d
beshik7 <tommypdx7@gmail.com>
2023-05-08 16:12:27 +0300

    Homework is done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            CredentialsValidator.validateCredentials("java_skypro_go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
+            System.out.println("Логин  и пароль введены верно.");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
